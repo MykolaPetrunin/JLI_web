14e5faa603bf5ab40f816fa46a0772a3aadb9e7d
Mykola Petrunin <petruninnikolay@gmail.com>
2022-12-02 18:00:57 +0100

    test

--- src/AppNavigation.tsx
+++ src/AppNavigation.tsx
@@ -29,7 +29,7 @@ const AppNavigation: FC = () => {
 
   if (isLoading) {
     return (
-      <Box height="100%" display="flex" alignItems="center" justifyContent="center">
+      <Box display="flex" height="100%" alignItems="center" justifyContent="center">
         <CircularProgress />
       </Box>
     );
